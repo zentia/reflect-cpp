37c4e24d19a2e86397c26521a32e36b50ad0364c
zentia <363476658@qq.com>
2025-04-02 20:19:19 +0800

    xmake

---
+++ xmake.lua
@@ -0,0 +1,13 @@
+target("reflect-cpp")
+    set_kind("$(kind)")
+    add_includedirs("include", {public = true})
+    add_includedirs("include/rfl/thirdparty", {public = true})
+    add_headerfiles(
+        "include/**.hpp",
+        "include/**.h"
+    )
+    add_files(
+        "src/reflectcpp.cpp", 
+        "src/reflectcpp_json.cpp", 
+        "src/yyjson.c"
+    )
